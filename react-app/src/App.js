9831d0df1043f724cb71d7304fe139ee548bf975
Jacob Grooms <grooms.jacob@gmail.com>
2020-12-30 14:02:10 -0500

    added custom 404 page, styling needed

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,7 @@ import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
+import NotFoundPage from "./components/NotFoundPage";
 // import { authenticate } from "./services/auth";
 
 function App() {
@@ -54,6 +55,9 @@ function App() {
 				<ProtectedRoute path="/" exact={true}>
 					<h1>My Home Page</h1>
 				</ProtectedRoute>
+				<Route path="*">
+					<NotFoundPage />
+				</Route>
 			</Switch>
 		</BrowserRouter>
 	);
