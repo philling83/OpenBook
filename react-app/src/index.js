44497a45c63b78eb32969d982494396c73f1a146
Jacob Grooms <grooms.jacob@gmail.com>
2020-12-30 15:59:16 -0500

    cleaning up some werid formatting

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter} from "react-router-dom"
+// import {BrowserRouter} from "react-router-dom"
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
@@ -11,9 +11,7 @@ const store = configureStore();
 function Root() {
 	return (
 		<Provider store={store}>
-	
-				<App />
-
+			<App />
 		</Provider>
 	);
 }
