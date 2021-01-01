ac486d39fd991b36189c37bfc2cd196f0b0ffcf9
brandon-perry <brandonperryofficial@gmail.com>
2021-01-01 18:29:31 -0500

    can now create classrooms and students at the same time, next is to allow pre-filling of items and ability to edit/delete classes

--- react-app/src/components/CreateClass.js
+++ react-app/src/components/CreateClass.js
@@ -1,14 +1,21 @@
 import React, {useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import './CreateClass.css'
+import * as classActions from '../store/classrooms'
 
 const CreateClass = () => {
     
+    const [className, setClassName] = useState('')
     const [names, setNames] = useState(['test']);
     const [password, setPassword] = useState('')
-    // const createdBy = useSelector(state => state.session.user.id);
+    // const teacherId = useSelector(state => state.session.user.id);
+    const teacherId = 1
     
     const dispatch = useDispatch();
+
+    const updateClassName = (e) => {
+        setClassName(e.target.value)
+    }
 
     const updateNames = (e) => {
         let temp_name = [...names]
@@ -36,6 +43,7 @@ const CreateClass = () => {
                     <input id={i} type='text' onChange={updateNames} value={el || ''} placeholder='Charlie R.' />
                     <button onClick={removeName}>Remove Student</button>
             </label>
+            
         ))
     }
 
@@ -43,6 +51,33 @@ const CreateClass = () => {
         e.preventDefault()
         setNames([...names, ''])
         console.log(names)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const classroomData = {
+            'name': className,
+            'password': password
+        }
+        console.log('classsroom data', classroomData)
+
+        const new_class = await dispatch(classActions.createRoom(teacherId, classroomData))
+        console.log(new_class)
+        const classroomId = new_class.id
+
+        const studentData = {
+            'list_of_students': names,
+            'classroom_id': classroomId
+        }
+
+        await fetch('/api/students/', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(studentData),
+        })
+
+
     }
     
     
@@ -52,12 +87,17 @@ const CreateClass = () => {
             <h1>Create class</h1>
     
             <form className='student-creation'> 
+                <label>
+                    Class Name
+                    <input type='text' placeholder="Molly's Class" onChange={updateClassName} />
+                </label>
                 {generateList()}
                 <button className='add-row' onClick={addRow}>Add Row</button>
                 <label>
                     Secret password for class login 
                     <input type='text' placeholder='Super-Secret12345' />
                 </label>
+                <button onClick={handleSubmit} >Create Your Class!</button>
             
             </form>
         </div>
