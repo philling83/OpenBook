375dda7b3bda9b505957f7fda070ef7ebc80fdd3
Philip <philipling83@gmail.com>
2021-01-06 21:09:11 -0500

    Comment out preloaded state

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -9,10 +9,10 @@ import { Provider } from "react-redux";
 import configureStore from "./store";
 
 
-const preloadedState = {
-	session: {user: {id: 1}},
-	deck: {deck: {cards: []}, decks: [], selected: null}
-}
+// const preloadedState = {
+// 	session: {user: {id: 1}},
+// 	deck: {deck: {cards: []}, decks: [], selected: null}
+// }
 
 const store = configureStore(preloadedState);
 
